84cbd7938746411fd7a0db760b17a58e40238cfd
BitHero <nuhumusah874@gmail.com>
2023-08-06 02:03:19 +0000

    Fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react';
+import Form from './components/form';
 
-const App = () => {
+class App extends Component {
+  render() {
+    return (
+      <div className='absolute flex justify-center items-center top-[50%] left-[50%] right-1/2 bottom-1/2'>
+        <Form />
+      </div>
+    );
+  }
+}
 
-}  
-
-export default App
+export default App;
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
+import Field from './fields';
 
 class Form extends Component {
-  
   state = {
-    fiels: {
+    fields : {
       name: '',
       email: '',
-      password: '',
-      confirmPassword: '',
-      message: ''
-    }
+    },
+    fieldErrors: {},
+    people : []
   }
+
+  onInputChange = ({ name, value, error }) => {
+    const fields = this.state.fields
+    const fieldErrors = this.state.fieldErrors
+    
+    fields[name] = value
+    fieldErrors[name] = error
+
+    this.setState({fields, fieldErrors})
+  }
+
+  validate = () => {
+    const person = this.state.fields
+    const fieldErrors = this.state.fieldErrors
+    const errorMessage = Object.keys(fieldErrors).filter((k) => fieldErrors[k])
+
+    if(!person.name) return true
+    if(!person.email) return true
+    if(errorMessage.length) return true
+
+    return false
+  }
+
+  onFormSubmit = (evt) => {
+    const people = this.state.people;
+    const person = this.state.fields;
+
+    evt.preventDefault();
+
+    if (this.validate()) return;
+
+    this.setState({
+      people: people.concat(person),
+      fields: {
+        name: '',
+        email: '',
+      },
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Sign up your Hooli Account here</h1>
-        <Field 
-          placeholder='Name'
-          name="name"
-          value={this.state.fields.name}
-          onChage={this.onInputChange}
-        />
+      <div className='flex flex-col justify-center items-center space-y-3'>
+        <h1 className='text-xl'>Sign up here</h1>
+        <form onSubmit={this.onFormSubmit}>
+          <Field 
+            placeholder='Name'
+            name="name"
+            value= {this.state.fields.name}
+            onChange={this.onInputChange}
+            validate={(val) => (val ? False: "Name is Required")}
+          />
+        </form>
       </div>
     );
   }
